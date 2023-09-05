b55b364b4d872d612ddb623e29b1eda14b838712
Daniel Cruz <daniel.jack.dmc@gmail.com>
2023-09-04 23:34:29 -0300

    ainda tentando

--- Pong/globals.py
+++ Pong/globals.py
@@ -9,6 +9,6 @@ FPS = 400
 WINDOW = pygame.display.set_mode(DISPLAY_SIZE)
 WINDOW_RECT = WINDOW.get_rect()
 
-HAVE_FILES = True  # ,False  # ,
-PLOT_RESULTS = True  # ,False  # ,
+HAVE_FILES = False  # ,True  # ,
+PLOT_RESULTS = False  # ,True  # ,
 # Caso queria treinar um novo conjunto de agentes Troque para False
